f666d96b72d16a0b962e5a2dab2a2d9d0bf7ce44
Hojin <suwer2696@gmail.com>
2026-09-19 16:09:49 +0900

    2일차 완료

--- python/Lesson-1/solution.py
+++ python/Lesson-1/solution.py
@@ -27,3 +27,52 @@ print('''Total days in Year
 365
 Circumference rate
 3.1415926535''')
+
+# 10.
+a = 97
+b = 13
+print("97 - 13 =", a - b)
+
+# 11.
+a = 3
+b = 'C'
+print(a)
+print(b)
+
+# 12.
+a = 26
+b = 5
+print("26 * 5 =", a * b)
+
+# 13.
+a, b, c = 7, 23, 30
+print("%d + %d = %d" %(a, b, c))
+
+# 14.
+a, b = 3, 'C'
+print("%d...%c" %(a, b))
+
+# 15.
+a, b = 3, 'C'
+print(f"{b}!.....!{a}")
+
+# 16.
+a, b, c = 1, 2, 'C'
+print(a, b, c, sep = "->")
+
+# 17.
+height, gravity = 13, 0.165
+print(f"{height} * {gravity:.6f} = {height * gravity:.6f}")
+
+# 18.
+a = 25.352
+print(f"{a:.1f}")
+
+# 19.
+oneFt, oneMi = 30.48, 160934
+print(f"9.2ft = {oneFt * 9.2:.1f}cm")
+print(f"1.3mi = {oneMi * 1.3:.1f}cm")
+
+# 20.
+a, b = 5.26, 8.27
+print(f"{a * b:.3f}")
